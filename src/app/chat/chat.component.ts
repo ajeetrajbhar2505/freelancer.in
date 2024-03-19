323999b92f5812afd4b626ef32db1b4279cbf52e
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-03-19 23:54:58 +0530

    integrated chat ui

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  activeClass = 'all-chat';
+  tabs: any[] = [
+    {
+      title: 'All Chats',
+      class  : 'all-chat'
+    },
+    {
+      title: 'Groups',
+      class  : 'group-chat'
+    },
+    {
+      title: 'Contacts',
+      class  : 'contact-chat'
+    },
+  ]
+
+  activateTab(index: any) {
+   this.activeClass =  this.tabs[index].class
+  }
 
 }
