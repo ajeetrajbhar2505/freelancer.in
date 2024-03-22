0a1c63ced63aacc527c133e1b2c7f1e871941c5b
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-03-23 02:10:15 +0530

    updated chat dashboard dynamic

--- src/app/chat/chat-routing.module.ts
+++ src/app/chat/chat-routing.module.ts
@@ -7,8 +7,8 @@ import { RoomComponent } from './room/room.component';
 const routes: Routes = [
   { path : '',redirectTo  : 'dashboard',pathMatch : 'full' },
   {path : 'dashboard',component : DashboardComponent},
-  {path : 'status',component : StatusComponent},
-  {path : 'room',component : RoomComponent},
+  {path : 'status/:userid',component : StatusComponent},
+  {path : 'room/:roomid',component : RoomComponent},
 ];
 
 @NgModule({
--- src/app/chat/dashboard/dashboard.component.ts
+++ src/app/chat/dashboard/dashboard.component.ts
@@ -6,43 +6,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
+  userDetails = { name : 'Ajeet'}
   activeClass = 'all-chat';
   activeIndex = 0
   tabs: any[] = [
     {
       title: 'All Chats',
-      class  : 'all-chat'
+      class: 'all-chat'
     },
     {
       title: 'Groups',
-      class  : 'group-chat'
+      class: 'group-chat'
     },
     {
       title: 'Contacts',
-      class  : 'contact-chat'
+      class: 'contact-chat'
     },
   ]
 
-  onlineUsers = [
-    { 
-      img : 'https://assets.materialup.com/uploads/0a167b5f-425d-4b90-adeb-57016ccbcbcd/0x0ss-85.jpg'
+  users = [
+    {
+      profile: 'https://img1.wsimg.com/isteam/ip/8f9a5028-92cb-4074-8d56-1467f1e7cf32/ajeetrajbhar.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:2000,h:2000,cg:true',
+      online: false,
+      verified : false,
+      userid: 0,
+      username : 'Ajeet Rajbhar (You)',
+      roomId: 99,
+      lastSeen : '12.34',
+      lastMessage : `Ok, Let me check`
     },
     {
-      img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2044px-WhatsApp.svg.png'
+      profile: 'https://assets.materialup.com/uploads/0a167b5f-425d-4b90-adeb-57016ccbcbcd/0x0ss-85.jpg',
+      online: true,
+      verified : true,
+      userid: 1,
+      username : 'Whatsapp',
+      roomId: 100,
+      lastSeen : 'Yesterday',
+      lastMessage : `Let's have meeting with you`
     },
     {
-      img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png'
+      profile: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2044px-WhatsApp.svg.png',
+      online: true,
+      verified : true,
+      userid: 2,
+      username : 'Telegram',
+      roomId: 101,
+      lastSeen : 'Yesterday',
+      lastMessage : `Let's have meeting with you`
     },
     {
-      img : 'https://freebiehive.com/wp-content/uploads/2021/08/Facebook-Icon-PNG-1.jpg'
+      profile: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png',
+      online: true,
+      verified : true,
+      userid: 3,
+      username : 'Facebook',
+      roomId: 102,
+      lastSeen : 'Yesterday',
+      lastMessage : `Let's have meeting with you`
+    },
+    {
+      profile: 'https://freebiehive.com/wp-content/uploads/2021/08/Facebook-Icon-PNG-1.jpg',
+      online: true,
+      verified : true,
+      userid: 4,
+      username : 'Snapchat',
+      roomId: 103,
+      lastSeen : 'Yesterday',
+      lastMessage : `Let's have meeting with you`
     }
   ]
 
+  get onlineUsers() {
+    return this.users.filter(u => u.online)
+  }
+
+  get totalunreadMessages() {
+    return this.users.length
+  }
+
+  get totalonlineUsers() {
+    return this.onlineUsers.length
+  }
+
   activateTab(index: any) {
     this.activeIndex = index
-   this.activeClass =  this.tabs[index].class
+    this.activeClass = this.tabs[index].class
   }
-  
+
   ngOnInit(): void {
     window.scrollTo(0, 0);
   }
